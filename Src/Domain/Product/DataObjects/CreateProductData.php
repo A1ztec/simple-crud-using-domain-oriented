c57e97910a34aac677f93daab2bfa6295e402bf4
Tarek Mohamed <A1ztec1@gmail.com>
2025-09-30 11:19:35 +0300

    fix(CreateProductData): make description optional in constructor

--- Src/Domain/Product/DataObjects/CreateProductData.php
+++ Src/Domain/Product/DataObjects/CreateProductData.php
@@ -8,7 +8,7 @@ class CreateProductData
 {
     public function __construct(
         public string $name,
-        public string $description,
+        public ?string $description = null,
         public float $price,
         public null|string|UploadedFile $image = null
     ) {}
