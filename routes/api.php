7da7808124945649538e470e7130e5dfcfe2d604
Tarek Mohamed <A1ztec1@gmail.com>
2025-10-01 12:43:54 +0300

    commented all the routes on my api route file

--- routes/api.php
+++ routes/api.php
@@ -10,19 +10,19 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::prefix('v1')->group(function () {
-    Route::prefix('auth')->controller(AuthController::class)->group(function () {
-        Route::post('/register', 'register')->name('api.auth.register');
-        Route::post('/login', 'login')->name('api.auth.login');
-        Route::post('/verify-email', 'verifyEmail')->name('api.auth.verify-email');
-        Route::post('/resend-verification-code', 'reSendVerificationCode')->name('api.auth.resend-verification-code');
-    });
+// Route::prefix('v1')->group(function () {
+//     Route::prefix('auth')->controller(AuthController::class)->group(function () {
+//         Route::post('/register', 'register')->name('api.auth.register');
+//         Route::post('/login', 'login')->name('api.auth.login');
+//         Route::post('/verify-email', 'verifyEmail')->name('api.auth.verify-email');
+//         Route::post('/resend-verification-code', 'reSendVerificationCode')->name('api.auth.resend-verification-code');
+//     });
 
-    Route::controller(ProductController::class)->prefix('products')->middleware('jwt.auth')->group(function () {
-        Route::get('/', 'listAll')->name('api.products.listAll');
-        Route::get('/{product}', 'show')->name('api.products.show');
-        Route::post('/', 'store')->name('api.products.save');
-        Route::post('/{product}', 'update')->name('api.products.update');
-        Route::delete('/{product}', 'destroy')->name('api.products.delete');
-    });
-});
+    // Route::controller(ProductController::class)->prefix('products')->middleware('jwt.auth')->group(function () {
+    //     Route::get('/', 'listAll')->name('api.products.listAll');
+    //     Route::get('/{product}', 'show')->name('api.products.show');
+    //     Route::post('/', 'store')->name('api.products.save');
+    //     Route::post('/{product}', 'update')->name('api.products.update');
+    //     Route::delete('/{product}', 'destroy')->name('api.products.delete');
+    // });
+// });
