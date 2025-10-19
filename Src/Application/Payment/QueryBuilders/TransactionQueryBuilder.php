e752d5fd4372be68d84f14f71c13a1a78325b7ef
Tarek Mohamed <A1ztec1@gmail.com>
2025-10-19 11:29:14 +0300

    refactor TransactionQueryBuilder to select specific columns in getTransactionByReferenceId method

--- Src/Application/Payment/QueryBuilders/TransactionQueryBuilder.php
+++ Src/Application/Payment/QueryBuilders/TransactionQueryBuilder.php
@@ -3,11 +3,15 @@
 namespace Application\Payment\QueryBuilders;
 
 use Domain\Payment\Models\Transaction;
+use Illuminate\Support\Facades\Schema;
 
 class TransactionQueryBuilder
 {
     public function getTransactionByReferenceId(string $referenceId): ?Transaction
     {
-        return Transaction::where('reference_id', $referenceId)->first();
+        $columns = Schema::getColumnListing('transactions');
+        $diffColumns = ['created_at', 'updated_at'];
+        $selectColumns = array_diff($columns, $diffColumns);
+        return Transaction::select($selectColumns)->where('reference_id', $referenceId)->first();
     }
 }
